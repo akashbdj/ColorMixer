9e0d953889730dd9e91004f9f614c65f97d775ff
akash-bhardwaj <akashbdj@gmail.com>
2016-10-01 23:50:13 +0530

    completed - That was easy

--- ColorMixer/ViewController.swift
+++ ColorMixer/ViewController.swift
@@ -1,20 +1,31 @@
-//
-//  ViewController.swift
-//  ColorMixer
-//
-//  Created by Akash on 01/10/16.
-//  Copyright © 2016 Akash. All rights reserved.
-//
+/**
+ *  ViewController.swift
+ *  ColorMixer
+ *
+ *  Created by Akash on 01/10/16.
+ *  Copyright © 2016 Akash. All rights reserved.
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-   
     }
-
+    
+    @IBAction func mixColor(_ sender: UISlider) {
+        let red   = self.redSlider.value
+        let green = self.greenSlider.value
+        let blue  = self.blueSlider.value
+        
+        result.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1)
+    }
    
 }
 
