d6f83087d3959a3a72fa137e33dbba968b9483c9
Akash <akash@webklipper.com>
2016-10-01 22:53:22 +0530

    Initial Commit

---
+++ ColorMixer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorMixer
+//
+//  Created by Akash on 01/10/16.
+//  Copyright © 2016 Akash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
